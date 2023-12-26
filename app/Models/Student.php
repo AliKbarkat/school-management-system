fdc38717a26edd50675a31168155961f2250e2b1
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-26 11:42:20 -0600

    add image model -m

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -10,30 +10,49 @@ class Student extends Model
     use HasTranslations;
     public $translatable = ['name'];
     public $guarded=[];
-//the relationship student with gender
+
+    //the relationship student with gender
     public function gender()
+
     {
+   
         return $this->belongsTo(Gender::class,'gender_id');
+
     }
    
-//the relationship student with grade
-public function grade()
-{
-    return $this->belongsTo(Grade::class,'grade_id');
-}
-//the relationship student with classroom
-public function classroom()
+    //the relationship student with grade
+    public function grade()
+
+    {
+
+        return $this->belongsTo(Grade::class,'grade_id');
+
+    }
+    //the relationship student with classroom
+    public function classroom()
+
     {
 
         return $this->belongsTo(Classroom::class,'classroom_id');
 
     }
-//the relationship student with Section
+    //the relationship student with Section
     
     public function Section()
+
+    {
+        
+        return $this->belongsTo(Section::class, 'section_id');
+    
+    }
+    //the relationship student with images
+    
+    public function images()
+
     {
 
-        return $this->belongsTo(Section::class, 'section_id');
+        return $this->morphMany(Image::class,'imageable');
 
-}
+    }
+
 }
