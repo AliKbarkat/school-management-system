abcaea274e5d514aa116b11f7782f28d7ee2322a
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 15:53:41 -0600

    create FeesRepositry and add function

---
+++ app/Repositry/FeesRepositry.php
@@ -0,0 +1,44 @@
+<?php
+use App\Models\Grade;
+use App\models\Graduted;
+use App\models\Promotion;
+use App\Models\Student;
+use App\Repositry\FeesInterface;
+use Illuminate\Support\Facades\DB;
+
+class FeesRepositry  implements FeesInterface
+{
+
+    public function indexFees()
+    {
+
+    }
+    public function createFees()
+    {
+
+    }
+    public function showFees($id)
+    {
+        
+    }
+    public function storeFees($request)
+    {
+
+    }
+  
+    public function editFees($id)
+    {
+
+    }
+  
+    public function updateFees($request)
+    {
+
+    }
+  
+    public function deleteFees($id)
+    {
+
+    }
+
+}
