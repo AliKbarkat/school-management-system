21cd38a8527250d84683d1add6353bc5f9f44730
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 17:18:57 -0600

    edit name header6

--- resources/views/fees/index.blade.php
+++ resources/views/fees/index.blade.php
@@ -10,7 +10,7 @@
 <div class="page-title">
     <div class="row">
         <div class="col-sm-6">
-            <h4 class="mb-0"> page empty</h4>
+            <h4 class="mb-0"> قائمة الرسوم </h4>
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb pt-0 pr-0 float-left float-sm-right ">
@@ -28,6 +28,37 @@
     <div class="col-md-12 mb-30">
         <div class="card card-statistics h-100">
             <div class="card-body">
+
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">{{__('grades.name_class_ar')}}</th>
+                        <th scope="col">{{__('grades.name_grade_ar')}}</th>
+                        <th scope="col">{{__('grades.procsess')}}</th>
+                     
+                      </tr>
+                    </thead>
+                    <tbody>
+                    
+                      <tr>
+                        <th scope="row"></th>
+
+                        <td><a href=""></a></td>
+                 
+                      <td></td> 
+                          
+                      <td><a class="btn btn-info btn-sm" href="">
+                        <i class="fa fa-edit"></i></a>
+                       <a class="btn btn-danger btn-sm" href=""  >
+                        <i class="fa fa-trash"></i></a></td>
+
+                      </tr>
+                        
+                     
+                    </tbody>
+             
+                  </table>
                 </div>
                 </div>
                 </div>
