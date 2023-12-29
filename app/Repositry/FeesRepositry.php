34233c69ed407b805c5dd02bf8ce168934956ff9
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 17:14:26 -0600

    update feesRepositry

--- app/Repositry/FeesRepositry.php
+++ app/Repositry/FeesRepositry.php
@@ -1,8 +1,10 @@
 <?php
+namespace App\Repositry;
+
+use App\Models\ClassRoom;
+use App\models\Fee;
 use App\Models\Grade;
-use App\models\Graduted;
-use App\models\Promotion;
-use App\Models\Student;
+use App\Models\Section;
 use App\Repositry\FeesInterface;
 use Illuminate\Support\Facades\DB;
 
@@ -11,24 +13,31 @@ class FeesRepositry  implements FeesInterface
 
     public function indexFees()
     {
-
+    $fees=Fee::all();
+    $grades=Grade::all();
+    return view('fees.index',compact('fees','grades'));
     }
     public function createFees()
     {
-
+        $Grades=Grade::all();
+        $class=ClassRoom::all();
+        $section=Section::all();
+        return view('fees.add_fees',compact('Grades','class','section'));
     }
-    public function showFees($id)
-    {
-        
-    }
+ 
     public function storeFees($request)
     {
+    try{
 
+
+        }catch(\Exception $e){
+
+        }
     }
   
     public function editFees($id)
     {
-
+        return view('fees.edit_fees');
     }
   
     public function updateFees($request)
