fa3ebd5613b05b5b2506c9c2061c93ab4a439269
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 07:46:02 -0600

    add relationship for student to promotion

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -6,5 +6,60 @@ use Illuminate\Database\Eloquent\Model;
 
 class Promotion extends Model
 {
-    //
+    public $guarded=[];
+
+    public function student()
+
+    {
+   
+        return $this->belongsTo(Student::class,'student_id');
+
+    }
+
+    public function f_grade()
+
+    {
+   
+        return $this->belongsTo(Grade::class,'from_grade');
+
+    }
+
+    public function f_class()
+
+    {
+   
+        return $this->belongsTo(ClassRoom::class,'from_class_room');
+
+    }
+    public function f_section()
+
+    {
+   
+        return $this->belongsTo(Section::class,'from_section');
+
+    }
+    public function to_grade()
+
+    {
+   
+        return $this->belongsTo(Grade::class,'from_grade');
+
+    }
+
+    public function to_class()
+
+    {
+   
+        return $this->belongsTo(ClassRoom::class,'from_class_room');
+
+    }
+    public function to_section()
+
+    {
+   
+        return $this->belongsTo(Section::class,'from_section');
+
+    }
+
+    
 }
