1c0abb1d1b8be18d6f338afe2188dc41a0012d67
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 07:56:44 -0600

    code js for footer-scripts

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -30,3 +30,29 @@
 <script src="{{ URL::asset('assets/js/lobilist.js') }}"></script>
 <!-- custom -->
 <script src="{{ URL::asset('assets/js/custom.js') }}"></script>
+
+<script>
+$(document).ready(function () {
+    $('select[name="grade_id"]').on('change', function () {
+        var Grade_id = $(this).val();
+        if (Grade_id) {
+            $.ajax({
+                url: "{{ URL::to('Get_classrooms') }}/" + Grade_id,
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    $('select[name="classroom_id"]').empty();
+                    $('select[name="classroom_id"]').append('<option selected disabled >{{trans('Parent_trans.Choose')}}...</option>');
+                    $.each(data, function (key, value) {
+                        $('select[name="classroom_id"]').append('<option value="' + key + '">' + value + '</option>');
+                    });
+                },
+            });
+        }
+        else {
+            console.log('AJAX load did not work');
+        }
+    });
+});
+</script>
+
