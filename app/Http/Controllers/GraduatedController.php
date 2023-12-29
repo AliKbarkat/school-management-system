0cb1e8511b2f0e8f88a73c4a62e3e9fe92d967b4
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 12:41:03 -0600

    add __counstract

--- app/Http/Controllers/GraduatedController.php
+++ app/Http/Controllers/GraduatedController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositry\GraduatedInterface;
 use Illuminate\Http\Request;
 
 class GraduatedController extends Controller
 {
-    //
+    protected $graduted;
+    public function __construct(GraduatedInterface $graduted){
+        $this->graduted=$graduted;
+    }
+  public function index(){
+    return $this->graduted->index();
+  }
+public function create(){
+    return $this->graduted->createGraduted();
+}
+public function store(Request $request){
+ 
+    return $this->graduted->softDelete($request);   
+}
+public function update(Request $request) {
+
+    return $this->graduted->returndData($request);
+}
+
+ public function destroy(Request $request)
+ {
+
+    return $this->graduted->destroy($request);
+}
+
 }
