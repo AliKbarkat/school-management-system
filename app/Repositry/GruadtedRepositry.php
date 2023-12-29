3d8d88772f12bdea9c68e46249399e8058bdaae7
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-29 12:51:43 -0600

    edit repositry

--- app/Repositry/GruadtedRepositry.php
+++ app/Repositry/GruadtedRepositry.php
@@ -10,88 +10,37 @@ use Illuminate\Support\Facades\DB;
 
 class GruadtedRepositry implements GraduatedInterface
 {
-  function getGraduted(){
-    $gruadted=Graduted::all();
-    return view('promotion.index',compact('$graduted'));
+  function index(){
+  
+     $student=Student::onlyTrashed()->get();
+     return view('promotion.index',compact('$graduted'));
   }
-  function createGraduted(){
-   $graduted=Graduted::all();
-    return view('promotion.add_promotion',compact('$graduted'));
+ public function createGraduted(){
+   $grades=Grade::all();
+    return view('Graduated.create',compact('grades'));
   }
-  function storeGraduted($request)
-  {
-    $students=Student::where('grade_id',$request->grade->id)->where('class_room_id',$request->classroom->id)->get();
-try{
-  DB::beginTransaction(); 
-  foreach($students as $student)
-  {
-    $ides=explode('.',$student->id);
-    $student::whereIn('id',$ides)
-      ->data([
-     'grade_id'=>$request->grade_id_new,
-     'class_room_id'=>$request->classroom_new,
-     'section_id'=>$request->section_id_new,
-  ]);
-
  
+  public function softDelete($request){
 
-  Promotion::creatOrupdate([
-    'student_id'=>$request->student_id,
-    'from_grade'=>$request->from_grade,
-    'from_Classroom'=>$request->from_grade,
-    'from_section'=>$request->from_section,
-    'to_grade'=>$request->to_grade,
-    'to_Classroom'=>$request->to_Classroom,
-    'to_section'=>$request->to_section,
-  ]);
-DB::commit();
+    $students=Student::where('grade_id',$request->grade_id)->where('classroom_id',$request->classroom_id)->where('section_id',$request->section_id);
+  
+    if($students->count() < 1){
+      return redirect()->back()->withErrors('error','لايوجد اية طلاب');
+    }
+   
+    foreach($students as $student){
+      $ides=explode('.',$student->id);
+    Student::whereIn('id',$ides)->Delete();
 }
-}catch(\Exception $e){
-  return $e;
-
-}
-       
-
-}
-  function editGraduted($id){
-return'edite';
+return redirect()->back();
   }
-  function GradutedUpdate($request){
-return 'update';
-  }
-  function deleteGraduted($request){
-    DB::beginTransaction();
-try{
-  if($request->page_id==1){
-    $promotions=Graduted::all();
-  foreach($promotions as $promotion){
-    $ides=explode('.',$promotion->student_id);
-    Student::whereIn('id',$ides)
-      ->data([
-     'grade_id'=>$promotion->from_grade,
-     'class_room_id'=>$promotion->from_Classroom,
-     'section_id'=>$promotion->from_section,
-  ]);
-
-  Promotion::truncate();
-  DB::commit();
+public function returndData($request){
+  Student::onlyTrashed()->where('id',$request->id)->restore();
   return redirect()->back();
-  }
-}
- else{
-  $promotion=Promotion::findOrfail($request->id);
-  Student::where('id',$promotion->student_id)
-      ->data([
-     'grade_id'=>$promotion->from_grade,
-     'class_room_id'=>$promotion->from_Classroom,
-     'section_id'=>$promotion->from_section,
-  ]);
-  Promotion::destroy($request->id);
 }
 
-}catch(\Exception $e){
-
+public function destroy($request){
+  Student::onlyTrashed()->where('id',$request->id)->forceDelete();
+  return redirect()->back();
 }
-  }
-
 }
