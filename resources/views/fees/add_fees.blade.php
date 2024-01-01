9861c29e49d49fa1fa18fbc52bbc862bf00a07c4
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 12:37:35 -0600

    edit file

--- resources/views/fees/add_fees.blade.php
+++ resources/views/fees/add_fees.blade.php
@@ -28,32 +28,34 @@
     <div class="col-md-12 mb-30">
         <div class="card card-statistics h-100">
             <div class="card-body">
+                <form method="post" action="{{ route('Fees.store') }}">
+                    @csrf
+
                <div class="form-row">
-
-                <div class="form-group col">
-                    <label for="">{{__('grades.name_class_en')}}</label><br>
-                    <input class="form-control form-control-md" type="text" name="name_class_en" 
-                    placeholder="{{__('grades.name_class_en')}}" aria-label=".form-control-lg example">
-                    @error('name_class_en')
+                 <div class="form-group col">
+                    <label for="">{{__('fees.title_fees_ar')}}</label><br>
+                    <input class="form-control form-control-md" type="text" name="title_ar" 
+                    placeholder="{{__('fees.title_fees_ar')}}" aria-label=".form-control-lg example">
+                    @error('title_fees_ar')
                     <small class="form-text text-danger" >{{$message}}</small> 
                     @enderror
                    
                 </div>
 
                 <div class="form-group col">
-                    <label for="">{{__('grades.name_class_en')}}</label><br>
-                    <input class="form-control form-control-md" type="text" name="name_class_en" 
-                    placeholder="{{__('grades.name_class_en')}}" aria-label=".form-control-lg example">
-                    @error('name_class_en')
+                    <label for="">{{__('fees.title_fees_en')}}</label><br>
+                    <input class="form-control form-control-md" type="text" name="title_en" 
+                    placeholder="{{__('fees.title_fees_en')}}" aria-label=".form-control-lg example">
+                    @error('title_fees_en')
                     <small class="form-text text-danger" >{{$message}}</small> 
                     @enderror
                    
                 </div>
                 <div class="form-group col">
-                    <label for="">{{__('grades.name_class_en')}}</label><br>
-                    <input class="form-control form-control-md" type="text" name="name_class_en" 
-                    placeholder="{{__('grades.name_class_en')}}" aria-label=".form-control-lg example">
-                    @error('name_class_en')
+                    <label for="">{{__('fees.ammount')}}</label><br>
+                    <input class="form-control form-control-md" type="text" name="ammount" 
+                    placeholder="{{__('fees.ammount')}}" aria-label=".form-control-lg example">
+                    @error('ammount')
                     <small class="form-text text-danger" >{{$message}}</small> 
                     @enderror
                    
@@ -63,7 +65,7 @@
                
                 <div class="form-row">
                     <div class="form-group col">
-                        <label for="">{{trans('student.grades')}}</label>
+                        <label for="">{{trans('fees.grades')}}</label>
                         <select class="custom-select my-1 mr-sm-2" name="grade_id">
                             <option selected>{{trans('student.Choose')}}...</option>
                              @foreach($Grades as $grade)
@@ -75,7 +77,7 @@
                         @enderror
                     </div>
                     <div class="form-group col">
-                        <label for="">{{trans('students.class_room')}}</label>
+                        <label for="">{{trans('fees.class_room')}}</label>
                         <select class="custom-select my-1 mr-sm-2" name="classroom_id">
                             <option selected>{{trans('my_parant.Choose')}}...</option>
                              @foreach($class as $class)
@@ -86,35 +88,54 @@
                         <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
                     </div>
+
+
                     <div class="form-group col">
                         <label for="">{{trans('my_parant.section_id')}}</label>
-                        <select class="custom-select my-1 mr-sm-2" name="section_id">
-                            <option selected>{{trans('my_parant.Choose')}}...</option>
-                            @foreach($section as $section)
-                                <option value="{{$section->id}}">{{$section->name_ar}}</option>
-                            @endforeach 
-                        </select>
-                        @error('section_id')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
+
+                         <select class="custom-select my-1 mr-sm-2" name="section_id">
+                             <option selected>{{trans('my_parant.Choose')}}...</option>
+                                @foreach($section as $section)
+                             <option value="{{$section->id}}">{{$section->name_ar}}</option>
+                                @endforeach 
+                         </select>
+                           @error('section_id')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                           @enderror
                     </div>
                 </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <label for="academic_year">{{trans('Students_trans.academic_year')}} : <span class="text-danger">*</span></label>
+                        <select class="custom-select mr-sm-2" name="year">
+                            <option selected disabled>{{trans('Parent_trans.Choose')}}...</option>
+                            @php
+                                $current_year = date("Y");
+                            @endphp
+                            @for($year=$current_year; $year<=$current_year +1 ;$year++)
+                                <option value="{{ $year}}">{{ $year }}</option>
+                            @endfor
+                        </select>
+                    </div>
+                </div>
+
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1">{{trans('Teacher.Address')}}</label>
-                    <input class="form-control" name="Address" id="exampleFormControlTextarea1">
-                    @error('Address')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                     <input class="form-control" name="descreption" id="exampleFormControlTextarea1">
+                      @error('Address')
+                       <div class="alert alert-danger">{{ $message }}</div>
+                      @enderror
                  </div>
-            </div>
+               </div>
            
-            <div>
+              <div>
                 <input type="submit" class="btn btn-success btn-sm btn-lg pull-right">
-            </div>
+              </div>
                 </div>
-                
+              
                 </div>
                 </div>
+            </form>
             </div>
         </div>
     </div>
