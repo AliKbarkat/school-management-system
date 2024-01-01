4d695e780c975e71151d1e5f58a254508a2a3707
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 12:34:38 -0600

    update storeFees and editFees and UpdateFees

--- app/Repositry/FeesRepositry.php
+++ app/Repositry/FeesRepositry.php
@@ -8,6 +8,7 @@ use App\Models\Section;
 use App\Repositry\FeesInterface;
 use Illuminate\Support\Facades\DB;
 
+
 class FeesRepositry  implements FeesInterface
 {
 
@@ -27,26 +28,73 @@ class FeesRepositry  implements FeesInterface
  
     public function storeFees($request)
     {
-    try{
 
+    try
+    {
+        $fees=new Fee();
+        $fees->title=['ar' => $request->title_ar ,'en' => $request->title_en ];
+        $fees->ammount=$request->ammount;
+        $fees->descreption=$request->descreption;
+        $fees->grade_id=$request->grade_id;        
+        $fees->classroom_id=$request->classroom_id;
+        $fees->section_id=$request->section_id;
+        $fees->year=$request->year;
+        $fees->save();
+        
+        toastr()->success('Data has been saved successfully!');
+
+        return redirect()->route('Fees.create');
 
         }catch(\Exception $e){
+            return redirect()->back()->withErrors(["error" => $e->getMessage()]);
 
         }
     }
   
     public function editFees($id)
+
     {
-        return view('fees.edit_fees');
+        $fee= Fee::findOrfail($id); 
+        $Grades=Grade::all();
+        $class=ClassRoom::all();
+        $section=Section::all();
+     
+       return view('fees.edit_fees',compact('Grades','fee','class','section'));
+        
     }
   
     public function updateFees($request)
     {
+        try{
+
+            $fees= Fee::findOrfail($request->id);
+            $fees->title=['ar' => $request->title_ar ,'en' => $request->title_en ];
+            $fees->ammount=$request->ammount;
+            $fees->descreption=$request->descreption;
+            $fees->grade_id=$request->grade_id;        
+            $fees->classroom_id=$request->classroom_id;
+            $fees->section_id=$request->section_id;
+            $fees->year=$request->year;
+            $fees->save();
+            toastr()->success('Data has been saved successfully!');
+
+         return redirect()->route('Fees.index');
+
+        }catch(\Exception $e){
+            return redirect()->back()->withErrors(["error" => $e->getMessage()]);
+
+        }
+       
+        
 
     }
   
-    public function deleteFees($id)
+    public function deleteFees($request)
     {
+        $teacher= Fee::findOrFail($request->id);
+        toaster()->error('the teacher not delete');
+        $teacher->delete();
+        return redirect()->route('teacher.index');
 
     }
 
