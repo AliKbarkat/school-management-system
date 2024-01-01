85fc0ad9ac1c4fc9a37230800e1e086414b36003
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 17:16:41 -0600

    add functions

--- app/Repositry/FeeInvoiceInterface.php
+++ app/Repositry/FeeInvoiceInterface.php
@@ -7,4 +7,11 @@ interface FeeInvoiceInterface{
     public function index();
 
     public function show($id);
+    public function store($request);
+
+    public function edit($id);
+
+    public function update($request);
+
+    public function delete($request);
 }
