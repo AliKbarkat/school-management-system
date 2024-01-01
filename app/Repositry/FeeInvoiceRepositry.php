662e199bf5957b02bede150811be718fe31cf92f
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 17:16:58 -0600

    add function

--- app/Repositry/FeeInvoiceRepositry.php
+++ app/Repositry/FeeInvoiceRepositry.php
@@ -3,6 +3,9 @@
 
 namespace App\Repositry;
 
+use App\models\Fee;
+use App\Models\Student;
+
 class FeeInvoiceRepositry  implements FeeInvoiceInterface
 {
     public function index(){
@@ -10,6 +13,9 @@ class FeeInvoiceRepositry  implements FeeInvoiceInterface
     }
 
     public function show($id){
-        
+        $student=Student::findOrfail($id);
+        $fee=Fee::where('clasroom_id',$student->clasroom_id)->get();
+        return view('fee_invoice.add',compact('student','fee'));
+
     }
 }
