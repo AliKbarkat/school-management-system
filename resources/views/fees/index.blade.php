debd560cb0799bad7a8e2608da54d7c84121e36c
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 12:38:14 -0600

    update table

--- resources/views/fees/index.blade.php
+++ resources/views/fees/index.blade.php
@@ -33,28 +33,37 @@
                     <thead>
                       <tr>
                         <th scope="col">#</th>
-                        <th scope="col">{{__('grades.name_class_ar')}}</th>
-                        <th scope="col">{{__('grades.name_grade_ar')}}</th>
-                        <th scope="col">{{__('grades.procsess')}}</th>
+                        <th scope="col">{{__('fees.title')}}</th>
+                        <th scope="col">{{__('fees.ammount')}}</th>
+                        <th scope="col">{{__('fees.grade')}}</th>
+                        <th scope="col">{{__('fees.classroom')}}</th>
+                        <th scope="col">{{__('fees.section')}}</th>
+                        <th scope="col">{{__('fees.year')}}</th>
+                        <th scope="col">{{__('fees.procsess')}}</th>
                      
                       </tr>
                     </thead>
                     <tbody>
-                    
+                      @foreach ($fees as $fee)
                       <tr>
-                        <th scope="row"></th>
-
-                        <td><a href=""></a></td>
-                 
-                      <td></td> 
-                          
-                      <td><a class="btn btn-info btn-sm" href="">
-                        <i class="fa fa-edit"></i></a>
-                       <a class="btn btn-danger btn-sm" href=""  >
-                        <i class="fa fa-trash"></i></a></td>
-
-                      </tr>
                         
+                        <td>{{$fee->id}}</td>
+                        <td>{{$fee->title}}</td>
+                        <td>{{$fee->ammount}}</td> 
+                        <td>{{$fee->grades->name_ar}}</td>
+                        <td>{{$fee->classes->name_class_ar}}</td>
+                        <td>{{$fee->section_id}}</td>
+                        <td>{{$fee->year}}</td>
+                        <td><a class="btn btn-info btn-sm" href="{{route('Fees.edit',$fee->id)}}">
+                          <i class="fa fa-edit"></i></a>
+                         <a class="btn btn-danger btn-sm" href=""  >
+                          <i class="fa fa-trash"></i></a></td>
+  
+                       
+   
+                    
+                      </tr>
+                      @endforeach
                      
                     </tbody>
              
