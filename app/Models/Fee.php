23cb46987f298b1e242fef16b19f31950e3c4c2b
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 12:25:41 -0600

    add function relationship

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -10,4 +10,13 @@ class Fee extends Model
     use HasTranslations;
     public $translatable = ['title'];
     public $guarded=[];
+
+    public function grades(){
+        return $this->belongsTo(Grade::class,'grade_id');
+    }
+    public function classes(){
+        return $this->belongsTo(ClassRoom::class,'classroom_id');
+
+    }
+
 }
