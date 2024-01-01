8c9e2519bb4b7d417617015ee56f215ccda407d0
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 16:03:35 -0600

    create FeeInvoiceInterface

---
+++ app/Http/Controllers/FeeInvoiceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositry\FeeInvoiceInterface;
+use Illuminate\Http\Request;
+
+class FeeInvoiceController extends Controller
+{
+    protected $fee_invoice;
+    public function __construct(FeeInvoiceInterface $fee_invoice){
+ 
+     $this->fee_invoice=$fee_invoice;
+   
+ }
+
+public function index(){
+    return $this->fee_invoice;
+}
+}
