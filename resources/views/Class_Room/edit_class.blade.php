207963ecc2191fdb28ceb1e31c94185e5a0c7379
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 12:35:33 -0600

    add route with link

--- resources/views/Class_Room/edit_class.blade.php
+++ resources/views/Class_Room/edit_class.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb pt-0 pr-0 float-left float-sm-right ">
-                <li class="breadcrumb-item"><a href="{{url('da')}}" class="default-color">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}" class="default-color">Home</a></li>
                 <li class="breadcrumb-item active">{{__('grades.edit')}}</li>
             </ol>
         </div>
