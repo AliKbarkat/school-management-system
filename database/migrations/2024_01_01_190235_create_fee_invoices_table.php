e8add5d8ef29b56788573909478be43121f03369
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 16:08:16 -0600

    CreateFeeInvoicesTable

---
+++ database/migrations/2024_01_01_190235_create_fee_invoices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeeInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fee_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->date('invoice_date');
+            $table->foreignId('grade_id')->references('id')->on('grades')->onDelete('cascade');
+            $table->foreignId('clasroon_id')->references('id')->on('class_rooms')->onDelete('cascade');
+            $table->foreignId('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreignId('fee_id')->references('id')->on('fees')->onDelete('cascade');
+            
+            $table->decimal('amount',8,2);
+            $table->string('descreption')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fee_invoices');
+    }
+}
