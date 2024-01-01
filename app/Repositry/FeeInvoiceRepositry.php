5bc1725773bbdf4e7540592dd3fc1b06d7d091d8
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-01 16:06:44 -0600

    create FeeInvoiceRepositry

---
+++ app/Repositry/FeeInvoiceRepositry.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Repositry;
+
+class FeeInvoiceRepositry  implements FeeInvoiceInterface
+{
+    public function index(){
+
+    }
+
+    public function show($id){
+        
+    }
+}
