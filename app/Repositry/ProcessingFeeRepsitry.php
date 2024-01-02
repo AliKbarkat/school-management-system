2f3d8da0a467ce9a45521e9a90500e57469bf104
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-02 07:40:42 -0600

    create ProcessingFeeRepsitry

---
+++ app/Repositry/ProcessingFeeRepsitry.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Repositry;
+
+class ProcessingFeeRepsitry implements ProcessingFeeInterface{
+
+    public function index(){
+        
+    }
+
+    public function show($id){
+        
+    }
+    public function store($request){
+        
+    }
+
+    public function edit($id){
+        
+    }
+
+    public function update($request){
+        
+    }
+
+    public function delete($request){
+        
+    }
+}
