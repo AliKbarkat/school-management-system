c01c9af1ab9faf3871c98a6fbd5594ac7272910d
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-02 15:58:32 -0600

    update

--- app/Repositry/PaymentRepositry.php
+++ app/Repositry/PaymentRepositry.php
@@ -3,31 +3,105 @@
 
 namespace App\Repositry;
 
+use App\models\FoundAcount;
+use App\models\PaymentStudent;
+use App\Models\Student;
+use App\models\StudentAccount;
+use Illuminate\Support\Facades\DB;
+
 class PaymentRepositry implements PaymentInterface{
  
     
     public function index()
     {
 
+        $student_payment=PaymentStudent::all();
+        return view('pament.index',compact('student_payment'));
     }
 
     public function show($id)
     {
 
+        $student=Student::findOrfail($id);
+        return view('payment.add',compact('student'));
+
     }
     public function store($request)
     {
+        DB::beginTransaction();
+ 
+        try{
+           $payment=new PaymentStudent();
+           $payment->date=date('Y-M-D');
+           $payment->student_id=$request->student_id;
+           $payment->amount=$request->debit;
+           $payment->description=$request->description;
+           $payment->save();
 
+           $faund_account=new FoundAcount();
+           $faund_account->date=date('y-m-d');
+           $faund_account->payment_id=$payment->id;
+           $faund_account->debit=0.00;
+           $faund_account->credit=$request->debit;
+           $faund_account->descreption=$request->descreption;
+           $faund_account->save();
+
+           $student_account=new StudentAccount();
+           $student_account->date=date('Y-M-D');
+           $student_account->tybe='recepit';
+           $student_account->student_id=$request->student_id;
+           $student_account->payment_id=$request->student_id;
+           $student_account->debit=0.00;
+           $student_account->descreption=$request->descreption;
+           $payment->save();
+           DB::commit();
+           return redirect()->route('Payment.index');
+        }catch(\Exception $e)
+        {
+
+        }
     }
 
     public function edit($id)
     {
-
+        $student_payment=PaymentStudent::findOrfail($id);
+        return view('pament.edit',compact('student_payment'));
     }
 
     public function update($request)
     {
-
+        DB::beginTransaction();
+ 
+        try{
+            $payment=PaymentStudent::findOrfail($request->id);
+            $payment->date=date('Y-M-D');
+            $payment->student_id=$request->student_id;
+            $payment->amount=$request->debit;
+            $payment->description=$request->description;
+            $payment->save();
+ 
+            $faund_account=FoundAcount::where('paymant_id',$request->id)->first();
+            $faund_account->date=date('y-m-d');
+            $faund_account->payment_id=$payment->id;
+            $faund_account->debit=0.00;
+            $faund_account->credit=$request->debit;
+            $faund_account->descreption=$request->descreption;
+            $faund_account->save();
+ 
+            $student_account=StudentAccount::where('paymant_id',$request->id)->first();
+            $student_account->date=date('Y-M-D');
+            $student_account->tybe='recepit';
+            $student_account->student_id=$request->student_id;
+            $student_account->payment_id=$request->student_id;
+            $student_account->debit=0.00;
+            $student_account->descreption=$request->descreption;
+            $payment->save();
+            DB::commit();
+            return redirect()->route('Payment.index');
+         }catch(\Exception $e)
+         {
+ 
+         }
     }
 
     public function delete($request)
