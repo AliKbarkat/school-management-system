d54e0c5eb6ade8e08afb41b5bfd13cee6c4f2663
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-02 16:12:25 -0600

    add function to AttendanceRepositry

---
+++ app/Repositry/AttendanceRepositry.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Repositry;
+
+class AttendanceRepositry  implements AttendanceInterface{
+    public function index(){
+        
+    }
+
+    public function show($id){
+        
+    }
+    public function store($request){
+        
+    }
+
+    public function edit($id){
+        
+    }
+
+    public function update($request){
+        
+    }
+
+    public function delete($request){
+        
+    }
+}
