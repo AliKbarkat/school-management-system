2477398f39851862c361dbe0469885937c5f620a
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-02 09:37:45 -0600

    create PaymentRepositry and add function

---
+++ app/Repositry/PaymentRepositry.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Repositry;
+
+class PaymentRepositry implements PaymentInterface{
+ 
+    
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+    public function store($request)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($request)
+    {
+
+    }
+
+    public function delete($request)
+    {
+
+    }  
+}
