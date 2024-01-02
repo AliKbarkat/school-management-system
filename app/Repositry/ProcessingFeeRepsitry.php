21eb92c7be4b51167cc5c4bdc0cf644b13a95bbb
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-02 09:25:56 -0600

    index,show, edit function

--- app/Repositry/ProcessingFeeRepsitry.php
+++ app/Repositry/ProcessingFeeRepsitry.php
@@ -1,26 +1,30 @@
 <?php
 namespace App\Repositry;
 
+use App\models\ProcessingFee;
 use App\Models\Student;
 
 class ProcessingFeeRepsitry implements ProcessingFeeInterface{
 
     public function index(){
-        
+        $processing=ProcessingFee::all();
+        return view('processing.index',compact('processing'));
     }
 
     public function show($id){
         $student=Student::findOrfail($id);
-        return view('',compact('$student'));
+        return view('',compact('student'));
     }
+    public function edit($id){
+        $processing=ProcessingFee::findOrFail($id);
+        return view('processing.edit',compact('processing'));
+    }
+
     public function store($request){
         
     }
 
-    public function edit($id){
-        
-    }
-
+   
     public function update($request){
         
     }
