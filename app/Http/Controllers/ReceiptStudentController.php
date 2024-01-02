34d62cdc70898b40e6389ff864f03bc8d5c93a65
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-02 09:10:36 -0600

    create ReceiptStudentController

---
+++ app/Http/Controllers/ReceiptStudentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositry\ReceiptStudentIntrface;
+use Illuminate\Http\Request;
+
+class ReceiptStudentController extends Controller
+{
+  protected $receipt;
+  public function __construct(ReceiptStudentIntrface $receipt)
+  {
+    $this->receipt=$receipt;
+  }
+
+  public function index()
+  {
+
+    return $this->receipt->index();
+
+  }
+  public function show($id)
+  {
+
+    return $this->receipt->show($id);
+  }
+  public function store($request)
+  {
+  
+    return $this->receipt->store($request);
+
+  }
+  public function update($request)
+  {
+    
+  }
+
+}
