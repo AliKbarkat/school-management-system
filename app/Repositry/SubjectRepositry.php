f3a5eefe57ae69edc1370f18910fc6cb84a4d7fa
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-03 02:28:25 -0600

    addfunction

---
+++ app/Repositry/SubjectRepositry.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Repositry;
+class SubjectRepositry implements SubjectInterface{
+    public function index(){
+        return "This is from the Subject Repository";
+    }
+
+    public function show($id){
+        //return $this->model->findOrFail($id);
+        
+    }
+    public function store($request){
+        /* $subject = new \App\Models\Subject();*/
+        
+    }
+
+    public function edit($id){
+        /*  $subject= $this->show($id);*/
+        
+    }
+
+    public function update($request){
+        /* $subject =$this->show($request->id);*/
+        
+    }
+
+    public function delete($request){
+        /*  $subject=$this->show($request->id);*/
+        
+    }
+}
