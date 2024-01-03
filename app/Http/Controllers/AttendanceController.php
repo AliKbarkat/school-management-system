338fa56c8a3099a2e80af942358a6a2aa60f4aa5
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-03 02:09:42 -0600

    add functions

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositry\AttendanceInterface;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    //
+    protected $attendance;
+    public function __construct(AttendanceInterface $attendance)
+    {
+      $this->attendance=$attendance;
+    }
+
+    public function index(){
+        return $this->attendance->index();
+    }
+
+    public function show($id){
+        return $this->attendance->show($id);
+    }
+
+    public function store(Request $request){
+        return $this->attendance->store($request);
+    }
 }
