56355fb692340183652dc64e52aff724cb241801
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-03 03:45:53 -0600

    update

--- app/Repositry/SubjectRepositry.php
+++ app/Repositry/SubjectRepositry.php
@@ -2,30 +2,67 @@
 
 
 namespace App\Repositry;
+
+use App\Models\Grade;
+use App\models\Subject;
+use App\models\Teacher;
+
 class SubjectRepositry implements SubjectInterface{
-    public function index(){
-        return "This is from the Subject Repository";
+    public function index()
+    {
+        $subjects=Subject::all();
+        return view('subjects.index',compact('subjects'));
+    
     }
 
     public function show($id){
         //return $this->model->findOrFail($id);
         
     }
-    public function create(){
-     /* if(!empty(request()->all())){*/
+    public function create()
+    {
+    $grades =Grade::get();
+    $teachers=Teacher::get();
+     return view('subjects.create',compact('grades','teachers'));
+    
     }
     public function store($request){
-        /* $subject = new \App\Models\Subject();*/
-        
+      try
+      {
+        $subject = new Subject();
+        $subject->name=['ar'=>$request->name_ar,'en'=>$request->name_en];
+        $subject->grade_id=$request->grade_id;
+        $subject->classroom_id=$request->classroom_id;
+        $subject->teacher_id=$request->teacher_id;
+        $subject->save();
+       
+      }catch(\Exception $e){
+    return redirect()->back()->withErrors($e);
+
+      }
     }
 
-    public function edit($id){
-        /*  $subject= $this->show($id);*/
-        
+    public function edit($id)
+    {
+        $subject=Subject::findOrFail($id);
+        return view('subjects.edit',compact('subject'));
+   
     }
 
     public function update($request){
-        /* $subject =$this->show($request->id);*/
+        try
+        {
+          $subject =Subject::findOrfail($request->id);
+          $subject->name=['ar'=>$request->name_ar,'en'=>$request->name_en];
+          $subject->grade_id=$request->grade_id;
+          $subject->classroom_id=$request->classroom_id;
+          $subject->teacher_id=$request->teacher_id;
+          $subject->save();
+         
+        }catch(\Exception $e){
+      return redirect()->back()->withErrors($e);
+  
+        }
         
     }
 
