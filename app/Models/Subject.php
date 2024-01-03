7b719eef33a462759b728487587fb1f15d269d89
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-03 03:45:24 -0600

    add fillable and relationship

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -3,8 +3,26 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Subject extends Model
 {
-    //
+    use HasTranslations;
+    public $translatable = ['name'];
+
+    public $fillable=['name','grade_id','classroom_id','teacher_id'];
+
+    public function grade()
+    {
+        return $this->BelongsTo(Grade::class,'grade_id');
+    }
+
+    public function clases()
+    {
+        return $this->BelongsTo(ClassRoom::class,'classroom_id');
+    }
+    public function teacher()
+    {
+        return $this->BelongsTo(Teacher::class,'teacher_id');
+    }
 }
