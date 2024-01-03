8ff9fe785c0072aded359cf0389e530e5ce1530b
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-03 02:27:44 -0600

    update

--- app/Providers/ReposerviceProvider.php
+++ app/Providers/ReposerviceProvider.php
@@ -8,60 +8,28 @@ class RepoServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(
+        $this->app->bind('App\Repositry\TeacherRepositryInterface','App\Repositry\TeacherRepositry');
 
-        'App\Repositry\TeacherRepositryInterface',
-        'App\Repositry\TeacherRepositry'
-    );
-    $this->app->bind(
-
-        'App\Repositry\StudentRepositryInterface',
-        'App\Repositry\StudentRepositry'
-    );
-   
+        $this->app->bind('App\Repositry\StudentRepositryInterface','App\Repositry\StudentRepositry');
     
-    $this->app->bind(
+        $this->app->bind('App\Repositry\PromotionInterface','App\Repositry\PromotionRepositry');
 
-        'App\Repositry\PromotionInterface',
-        'App\Repositry\PromotionRepositry'
-    );
+        $this->app->bind('App\Repositry\GraduatedInterface','App\Repositry\GruadtedRepositry');
+  
+        $this->app->bind('App\Repositry\FeesInterface','App\Repositry\FeesRepositry');
+  
+        $this->app->bind('App\Repositry\FeeInvoiceInterface','App\Repositry\FeeInvoiceRepositry');
 
-    $this->app->bind(
+        $this->app->bind('App\Repositry\ReceiptStudentIntrface','App\Repositry\ReceiptStudentRepositry');
+   
+        $this->app->bind('App\Repositry\ProcessingFeeInterface', 'App\Repositry\ProcessingFeeRepositry');
+    
+        $this->app->bind('App\Repositry\PaymentInterface','App\Repositry\PaymentRepositry');
 
-        'App\Repositry\GraduatedInterface',
-        'App\Repositry\GruadtedRepositry'
-    );
-    $this->app->bind(
+        $this->app->bind( 'App\Repositry\AttendanceInterface','App\Repositry\AttendanceRepositry');
 
-        'App\Repositry\FeesInterface',
-        'App\Repositry\FeesRepositry'
-    );
-    $this->app->bind(
+        $this->app->bind( 'App\Repositry\SubjectInterface','App\Repositry\SubjectRepositry');
 
-        'App\Repositry\FeeInvoiceInterface',
-        'App\Repositry\FeeInvoiceRepositry'
-    );
-
-    $this->app->bind(
-
-        'App\Repositry\ReceiptStudentIntrface',
-        'App\Repositry\ReceiptStudentRepositry'
-    );
-    $this->app->bind(
-
-        'App\Repositry\ProcessingFeeInterface',
-        'App\Repositry\ProcessingFeeRepositry'
-    );
-    $this->app->bind(
-
-        'App\Repositry\PaymentInterface',
-        'App\Repositry\PaymentRepositry'
-    );
-    $this->app->bind(
-
-        'App\Repositry\AttendanceInterface',
-        'App\Repositry\AttendanceRepositry'
-    );
     }
     public function boot()
     {
