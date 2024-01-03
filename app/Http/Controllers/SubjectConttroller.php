2313029fd34e773625060e73b4033113c8842277
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2024-01-03 02:47:08 -0600

    add construct and functions

--- app/Http/Controllers/SubjectConttroller.php
+++ app/Http/Controllers/SubjectConttroller.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositry\SubjectInterface;
 use Illuminate\Http\Request;
 
 class SubjectConttroller extends Controller
 {
-    //
+    protected $subject;
+    function __construct(SubjectInterface $subject)
+    {
+        $this->subject = $subject;
+    }
+    public function index(){
+return $this->subject->index();
+    }
+    public function show($id){
+        return $this->subject->show($id);
+    }
+    public function create(){
+        return $this->subject->create();
+    }
+    public function update(Request $request){
+        return $this->subject->update($request);
+    }
+    public function destroy($request){
+        return $this->subject->delete($request);
+    }
 }
