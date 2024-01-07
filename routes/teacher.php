e836e5ecf4e8c0e22c37d784d7798770223e8acf
AliKbarkat <ali.khaled.dev3@gmail.com>
2024-01-06 20:31:57 -0600

    teacher routes

---
+++ routes/teacher.php
@@ -0,0 +1,25 @@
+<?php
+use App\Http\Controllers\HomeController;
+use App\Models\Student;
+use App\models\Teacher;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect',
+        'localizationRedirect', 'localeViewPath',
+        'auth:teacher']
+    ], function () {
+
+    //==============================dashboard============================
+    Route::get('/teacher/dashboard', function () {
+        $tea_id = auth()->user()->id;
+        $ids = Teacher::findOrfail($tea_id)->Sections()->pluck('section_id');
+        $data['count_section'] = $ids->count();
+        $data['count_students'] = Student::where('section_id',$ids)->count();
+        return view('pages.Teachers.dashboard.dashboard',$data);
+    });
+
+});
