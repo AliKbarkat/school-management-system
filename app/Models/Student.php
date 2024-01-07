0424d9bb48d8a709d0636df85687d3749454f90c
AliKbarkat <ali.khaled.dev3@gmail.com>
2024-01-06 20:22:00 -0600

    Authenticatable

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Translatable\HasTranslations;
 
-class Student extends Model
+class Student extends Authenticatable
 {
     use SoftDeletes;
     use HasTranslations;
