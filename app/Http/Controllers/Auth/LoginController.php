559755111f9c071cf98fa3ef41be89aa631d3389
AliKbarkat <ali.khaled.dev3@gmail.com>
2024-01-06 20:19:09 -0600

    change login for user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\AuthTrait;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -19,13 +22,34 @@ class LoginController extends Controller
     |
     */
 
-    //  use AuthenticatesUsers;
+      // use AuthenticatesUsers;
+      use AuthTrait;
 
-  public function loginFom($type){
-    return view('auth.login',compact('tybe'));
-  }
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+  public function loginFrom($type){
+  
+     return view('auth.login',compact('type'));
+
+  }
+  public function login(Request $request){
+
+    if (Auth::guard($this->chekGuard($request))->attempt(['email' => $request->email, 'password' => $request->password])) {
+    return $this->redirect($request);
+    }
+    else{
+        return redirect()->back()->with('message','هناك خطأ في كلمة المرور او اسم المستخدم');
+    }
+// return $request;
+}
+public function logout(Request $request,$type)
+    {
+        Auth::guard($type)->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
