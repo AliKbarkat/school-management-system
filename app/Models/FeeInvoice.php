1ff5d54a2837ae2c24ff7c791484b21ccd327152
AliKbarkat <ali.khaled.dev3@gmail.com>
2024-01-06 20:21:31 -0600

    change name file

---
+++ app/Models/FeeInvoice.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FeeInvoice extends Model
+{
+    public $fillable =['id','invoice_date','grade_id','clasroon_id','student_id','fee_id','amount','descreption'];
+public function grades(){
+    return $this->belongsTo(Grade::class,'grade_id');
+}
+public function classes(){
+    return $this->belongsTo(ClassRoom::class,'clasroon_id');
+}
+public function studens(){
+    return $this->belongsTo(Student::class,'student_id');
+}
+public function fees(){
+    return $this->belongsTo(Fee::class,'fee_id');
+}
+
+
+}
+
